47b3df77e922598d2bc279bb08426cec9f9d3558
Corey Johnson <johnsoncorcwu@gmail.com>
2020-08-30 13:26:10 -0700

    Prepare for Heroku

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,10 +1,18 @@
 require("dotenv").config();
 const request = require("request");
 
-const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+// const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+const WEATHER_API_KEY = ab4bb12db7014af34d69c8fd49d08cc0;
 
 const forcast = (latitude, longitude, callback) => {
-  const url = `http://api.weatherstack.com/current?access_key=${WEATHER_API_KEY}&query=${latitude},${longitude}&units=f`;
+  const url =
+    "http://api.weatherstack.com/current?access_key=" +
+    WEATHER_API_KEY +
+    "&query=" +
+    latitude +
+    "," +
+    longitude +
+    "&units=f";
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,10 +1,18 @@
 require("dotenv").config();
 const request = require("request");
 
-const GEO_API_KEY = process.env.GEO_API_KEY;
+// const GEO_API_KEY = process.env.GEO_API_KEY;
+const GEO_API_KEY =
+  pk.eyJ1Ijoiam9obnNvbmNvcmN3dSIsImEiOiJja2VjNjVyb20wNGozMnFzNGUwOHVodGNsIn0
+    .T_IgOxVMGInSR9IkyVN8XA;
 
 const geocode = (address, callback) => {
-  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${GEO_API_KEY}&limit=1`;
+  const url =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    address +
+    ".json?access_token=" +
+    GEO_API_KEY +
+    "&limit=1";
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
