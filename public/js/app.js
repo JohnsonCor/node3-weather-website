96034fb259e8117a04d01fc0e427567fdc445eea
Corey Johnson <johnsoncorcwu@gmail.com>
2020-08-30 12:59:31 -0700

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,19 +11,17 @@ weatherForm.addEventListener("submit", (e) => {
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
 
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          // console.log(data.error);
-          messageOne.textContent = data.error;
-        } else {
-          // console.log(data.location);
-          // console.log(data.forcast);
-          messageOne.textContent = data.location;
-          messageTwo.textContent = data.forcast;
-        }
-      });
-    }
-  );
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        // console.log(data.error);
+        messageOne.textContent = data.error;
+      } else {
+        // console.log(data.location);
+        // console.log(data.forcast);
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forcast;
+      }
+    });
+  });
 });
